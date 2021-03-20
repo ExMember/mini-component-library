2d0ee8e43ae10e05560bd78674ccb8d8871b0de3
Damien Burke <damien@damienburke.com>
2021-03-20 10:12:46 -0700

    Create a styled well for the progress bar to sit in

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,13 +5,21 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <div
+  return <Wrapper
     role="progressbar"
     aria-valuenow="{value}"
     aria-valuemin="0"
     aria-valuemax="100">
     <VisuallyHidden>{value} %</VisuallyHidden>
-  </div>;
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  padding: 0;
+  background-color: ${ COLORS.transparentGray15 };
+  border-radius: 4px;
+  box-shadow: 0 1px 4px 0 ${ COLORS.transparentGray35 } inset;
+  height: 12px;
+`;
 
 export default ProgressBar;
