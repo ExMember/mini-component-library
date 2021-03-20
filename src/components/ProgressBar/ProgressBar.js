21cb58779aa6455d32b4a1108560f85ea904628f
Damien Burke <damien@damienburke.com>
2021-03-20 10:29:38 -0700

    Add moveable indicator

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -10,6 +10,7 @@ const ProgressBar = ({ value, size }) => {
     aria-valuenow="{value}"
     aria-valuemin="0"
     aria-valuemax="100">
+    <Indicator value={value}/>
     <VisuallyHidden>{value} %</VisuallyHidden>
   </Wrapper>;
 };
@@ -18,8 +19,16 @@ const Wrapper = styled.div`
   padding: 0;
   background-color: ${ COLORS.transparentGray15 };
   border-radius: 4px;
-  box-shadow: 0 1px 4px 0 ${ COLORS.transparentGray35 } inset;
+  box-shadow: 0 1px 4px 0 ${ COLORS.transparentGray15 } inset;
   height: 12px;
+`;
+
+const Indicator = styled.div`
+  padding: 0;
+  background-color: ${ COLORS.primary };
+  border-radius: 4px;
+  height: 13px;
+  width: ${props => props.value}%;
 `;
 
 export default ProgressBar;
