13b6b4a54ed26eb25fe2741d661296b9da63f0db
Damien Burke <damien@damienburke.com>
2021-03-20 15:25:50 -0700

    Create styled select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,40 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <InvisibleSelect value={value} onChange={onChange}>
+        {children}
+      </InvisibleSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.span`
+  background-color: ${ COLORS.transparentGray15 };
+  color: ${ COLORS.gray700 };
+  border-radius: 8px;
+  display: inline-block;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+const InvisibleSelect = styled.select`
+  appearance: none;
+  background-color: transparent;
+  border: none;
+  color: inherit;
+  font-family: inherit;
+  font-style: inherit;
+  font-weight: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  margin: 0;
+  padding: 12px 16px;
+  outline: none;
+  width: auto;
+`;
 
 export default Select;
