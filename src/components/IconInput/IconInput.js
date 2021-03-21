9cc8b6299549253b26cf43f5c129b146c14646e7
Damien Burke <damien@damienburke.com>
2021-03-20 18:14:44 -0700

    Style typography

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,14 +13,37 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-return <div>
+return <Wrapper>
   <Label for="{label}">{label}</Label>
-  <input id="{label}" type="text" placeholder={placeholder}></input>
-</div>;
+  <Input id="{label}" type="text" placeholder={placeholder}></Input>
+</Wrapper>;
 };
+
+const Wrapper = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 1.25;
+`;
 
 const Label = styled.label`
   display: none;
 `;
+
+const Input = styled.input`
+  color: ${COLORS.gray700};
+  font-weight: bold;
+  font-size: inherit;
+  line-height: inherit;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: normal;
+    font-size: inherit;
+    line-height: inherit;
+  }
+`;
+
 
 export default IconInput;
